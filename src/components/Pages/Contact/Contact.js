42534c7afb95e65e7722b17de0dc59ea2b6ebbf2
angeleraser <angeleraser@gmail.com>
2020-08-17 00:21:45 -0400

    Fix footer element margin, iframe propertys

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -57,13 +57,13 @@ const Contact = () => {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.685959157122!2d-63.54192132929875!3d8.13341880846893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8dce871c9a329ce5%3A0x8458b31c01e2157f!2sCoorporacion%20Clinica%20Universitaria%20De%20Oriente!5e0!3m2!1ses-419!2sve!4v1597637575925!5m2!1ses-419!2sve"
               width="100%"
               height="450"
-              frameborder="0"
+              frameBorder="0"
               style={{
                 border: "0px",
               }}
-              allowfullscreen=""
+              allowFullScreen=""
               aria-hidden="false"
-              tabindex="0"></iframe>
+              tabIndex="0"></iframe>
           </li>
         </ul>
       </section>
